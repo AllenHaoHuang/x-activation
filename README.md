e78976d21c62f64d44baf961124c8d783e0e183f
Allen Hao Huang <allenhuangdd@gmail.com>
2024-06-06 19:32:59 +1200

    x-gate package

---
+++ README.md
@@ -0,0 +1,120 @@
+# Expanded Gating Ranges Improve Activation Functions
+
+## About
+
+This repository contains implementations of the proposed activation functions and gated linear units proposed in the
+paper [Expanded Gating Ranges Improve Activation Functions](https://arxiv.org/abs/2405.20768).
+
+Key Results
+
+- For **standard MLP**, XATLU > XGELU / XSiLU > GELU / SiLU
+- For **first-order GLU**, XATGLU > XGEGLU / XSwiGLU > ATGLU / GEGLU / SwiGLU
+- For **second-order GLU**, expanded gating ranges does not improve performance, just use ATGLU / GEGLU / SwiGLU
+- First-order GLU with expanded gating ranges appears to match the performance of second-order GLU
+
+## Installation
+
+The code is arranged as a `x_gate` package. To install the `x_gate` package, run:
+
+```
+pip install -e .
+```
+
+Requirements:
+
+- PyTorch 1.12+
+
+## Usage
+
+Code snippet for standard MLP using XATLU / XGELU / XSiLU
+
+```python
+import torch.nn as nn
+from x_gate import XATLU, XGELU, XSiLU
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+        self.activation = XATLU()
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.activation(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+```
+
+Code snippet for gated MLP using XATGLU / XGEGLU / XSwiGLU
+
+```python
+import torch.nn as nn
+from x_gate import XATGLU, XGEGLU, XSwiGLU
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, int(2 * 8 / 3 * config.n_embd), bias=config.bias)
+        self.c_proj = nn.Linear(int(8 / 3 * config.n_embd), config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+        self.activation = XATGLU(order=1)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.activation(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+```
+
+Code snippet for gated MLP directly using the expanded gating function XAT / XGE / XS
+
+```python
+import torch.nn as nn
+from x_gate import XAT, XGE, XS
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, int(2 * 8 / 3 * config.n_embd), bias=config.bias)
+        self.c_proj = nn.Linear(int(8 / 3 * config.n_embd), config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+        self.gate = XAT()
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x, y = x.chunk(2, dim=-1)
+        # first order GLU
+        x = self.gate(x) * y
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+```
+
+## Additional Experiments / TODO
+
+- Experimental changes on nanoRWKV using arctan based gating
+  for [12 layers](https://api.wandb.ai/links/saesara/w8cny2aj), [24 layers](https://api.wandb.ai/links/saesara/f7s881y2)
+- Running larger scale experiments using per channel weights instead of a scalar
+- Second order GLU appears to marginally improve (15.57 ppl -> 15.50 ppl) by scaling the range of the gating
+  function to (α, β) and will be added if the improvements are consistent
+
+## Citation
+
+```
+@misc{huang2024expanded,
+      title={Expanded Gating Ranges Improve Activation Functions}, 
+      author={Allen Hao Huang},
+      journal={arXiv preprint arXiv:2405.20768}
+      year={2024}
+}
+```
---
+++ src/x_gate.py
@@ -0,0 +1,140 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+class XAT(nn.Module):
+
+    def __init__(self, param_size=1):
+        super(XAT, self).__init__()
+        self.alpha = nn.Parameter(torch.zeros(param_size))
+        self.half_pi = math.pi / 2
+        self.inv_pi = 1 / math.pi
+
+    def forward(self, x):
+        gate = torch.arctan(x)
+        return gate * (self.inv_pi * (1 + 2 * self.alpha)) + 0.5
+
+
+class XGE(nn.Module):
+
+    def __init__(self, param_size=1):
+        super(XGE, self).__init__()
+        self.alpha = nn.Parameter(torch.zeros(param_size))
+        self.inv_sqrt2 = 1 / math.sqrt(2)
+
+    def forward(self, x):
+        gate = torch.erf(x * self.inv_sqrt2)
+        return gate * (0.5 + self.alpha) + 0.5
+
+
+class XS(nn.Module):
+
+    def __init__(self, param_size=1):
+        super(XS, self).__init__()
+        self.alpha = nn.Parameter(torch.zeros(param_size))
+
+    def forward(self, x):
+        gate = torch.sigmoid(x)
+        return gate * (1 + 2 * self.alpha) - self.alpha
+
+
+class XATLU(nn.Module):
+
+    def __init__(self, param_size=1):
+        super(XATLU, self).__init__()
+        self.gate = XAT(param_size)
+
+    def forward(self, x):
+        return self.gate(x) * x
+
+
+class XGELU(nn.Module):
+
+    def __init__(self, param_size=1):
+        super(XGELU, self).__init__()
+        self.gate = XGE(param_size)
+
+    def forward(self, x):
+        return self.gate(x) * x
+
+
+class XSiLU(nn.Module):
+
+    def __init__(self, param_size=1):
+        super(XSiLU, self).__init__()
+        self.gate = XS(param_size)
+
+    def forward(self, x):
+        return self.gate(x) * x
+
+
+class ATGLU(nn.Module):
+
+    def __init__(self, order=1):
+        super(ATGLU, self).__init__()
+        assert order in [1, 2], "order must be either 1 or 2"
+        self.order = order
+        self.half_pi = math.pi / 2
+        self.inv_pi = 1 / math.pi
+
+    def forward(self, x):
+        assert x.shape[-1] % 2 == 0, "dimension must be even"
+        x, y = x.chunk(2, dim=-1)
+        gate = (torch.arctan(x) + self.half_pi) * self.inv_pi
+        if self.order == 1:
+            return gate * y
+        else:
+            return gate * x * y
+
+
+class XATGLU(nn.Module):
+
+    def __init__(self, order=1, param_size=1):
+        super(XATGLU, self).__init__()
+        assert order in [1, 2], "order must be either 1 or 2"
+        self.order = order
+        self.gate = XAT(param_size)
+
+    def forward(self, x):
+        assert x.shape[-1] % 2 == 0, "dimension must be even"
+        x, y = x.chunk(2, dim=-1)
+        if self.order == 1:
+            return self.gate(x) * y
+        else:
+            return self.gate(x) * x * y
+
+
+class XGEGLU(nn.Module):
+
+    def __init__(self, order=1, param_size=1):
+        super(XGEGLU, self).__init__()
+        assert order in [1, 2], "order must be either 1 or 2"
+        self.order = order
+        self.gate = XGE(param_size)
+
+    def forward(self, x):
+        assert x.shape[-1] % 2 == 0, "dimension must be even"
+        x, y = x.chunk(2, dim=-1)
+        if self.order == 1:
+            return self.gate(x) * y
+        else:
+            return self.gate(x) * x * y
+
+
+class XSwiGLU(nn.Module):
+
+    def __init__(self, order=1, param_size=1):
+        super(XSwiGLU, self).__init__()
+        assert order in [1, 2], "order must be either 1 or 2"
+        self.order = order
+        self.gate = XS(param_size)
+
+    def forward(self, x):
+        assert x.shape[-1] % 2 == 0, "dimension must be even"
+        x, y = x.chunk(2, dim=-1)
+        if self.order == 1:
+            return self.gate(x) * y
+        else:
+            return self.gate(x) * x * y
